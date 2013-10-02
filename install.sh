3cbbafec84a9ad6e115c9c874e8e1a8e13af43b0
Ben Pham <pckben@gmail.com>
2013-10-02 04:10:09 -0700

    git config, rupa/z jumper

--- install.sh
+++ install.sh
@@ -1,10 +1,11 @@
-while read i
-do git submodule add $i
-done < modules
-
 git submodule init
 git submodule update
+
+# For rupa-z jumper
+mkdir .z
 
 ln -s ~/dotfiles/zsh/zshrc ~/.zshrc
 ln -s ~/dotfiles/vim/vimrc ~/.vimrc
 ln -s ~/dotfiles/vim ~/.vim
+ln -s ~/dotfiles/git/gitignore_global ~/.gitignore_global
+ln -s ~/dotfiles/git/gitconfig ~/.gitconfig
