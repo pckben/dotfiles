d91874b9bca1ef3d5cfbd67f720840d3a12eed9b
Ben Pham <pckben@gmail.com>
2013-10-06 05:55:11 -0700

    git config global

--- install.sh
+++ install.sh
@@ -7,5 +7,7 @@ mkdir .z
 ln -s ~/dotfiles/zsh/zshrc ~/.zshrc
 ln -s ~/dotfiles/vim/vimrc ~/.vimrc
 ln -s ~/dotfiles/vim ~/.vim
-ln -s ~/dotfiles/git/gitignore_global ~/.gitignore_global
 ln -s ~/dotfiles/git/gitconfig ~/.gitconfig
+ln -s ~/dotfiles/git/gitignore_global ~/.gitignore_global
+
+git config --global core.excludesfile ~/.gitignore_global
